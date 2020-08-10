36bcde8d61cc0fa74e7f06b9e21f6645404ebb3b
DESKTOP-OTE8R7F\yjh13 <yjh135908@gmail.com>
2020-08-11 08:38:07 +0900

    delete pingpong

--- main.go
+++ main.go
@@ -13,11 +13,6 @@ func main() {
 	ThemeHelper.InitStatic()
 	r := gin.Default()
 
-	//PingPong
-	r.GET("/ping", func(context *gin.Context) {
-		context.String(200, "pong!")
-	})
-
 	//Boot server
 	Router.Setup(r, SettingHelper.ReadSetting("wiki", "name"), SettingHelper.ReadSetting("wiki", "start_page"))
 	r.Run(":80")
